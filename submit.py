86d293132ecfc714675e14a0833d985887b55ea9
David Kim <jk2537@cornell.edu>
2021-12-06 22:18:08 -0500

    Further improved api

--- submit.py
+++ submit.py
@@ -1,12 +1,14 @@
 import time
+import argparse
 
 import load_data as ld
 import models
 
 # ML algorithms
 LINREG = models.LINREG
-FORESTS = models.FORESTS
 ADABOOST = models.ADABOOST
+FORESTS = models.FORESTS
+algorithms = [LINREG, ADABOOST, FORESTS]
 
 
 def _name_of_algorithm(algorithm):
@@ -18,7 +20,7 @@ def _name_of_algorithm(algorithm):
         return "AdaBoost"
 
 
-def train_and_predict(xTr, yTr, xVal, yVal, xTe, algorithm=FORESTS):
+def train_and_predict(xTr, yTr, xVal, yVal, xTe, algorithm):
     # Impute the missing (NaN) data from the dataset
     xTr, yTr = ld.prune(xTr, yTr, remove_rows=False)
     xVal, yVal = ld.prune(xVal, yVal, remove_rows=False)
@@ -28,9 +30,9 @@ def train_and_predict(xTr, yTr, xVal, yVal, xTe, algorithm=FORESTS):
     # Train and build a model
     print("Training model...")
     start_time = time.time()
-    model = models.train(xTr, yTr, FORESTS)
+    model = models.train(xTr, yTr, algorithm)
     elapsed_time = time.time() - start_time
-    print(f"Model trained with {_name_of_algorithm(FORESTS)}\n"
+    print(f"Model trained with {_name_of_algorithm(algorithm)}\n"
           f"Time elapsed: {elapsed_time} seconds")
 
     # Validate the trained model
@@ -51,9 +53,26 @@ def submit(pred):
                               index_label="id")
 
 
+def _init_argparse():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawTextHelpFormatter)
+    help_str = ""
+    for a in algorithms:
+        help_str += f"({a}) {_name_of_algorithm(a)}\n"
+    parser.add_argument("-a",
+                        "--algorithm",
+                        default=LINREG,
+                        type=int,
+                        help=help_str)
+    return parser.parse_args()
+
+
 def main():
+    args = _init_argparse()
+    assert args.algorithm in algorithms, "No such algorithm"
     xTr, yTr, xVal, yVal, xTe = ld.load_data()
-    model, val_score, test_pred = train_and_predict(xTr, yTr, xVal, yVal, xTe)
+    model, val_score, test_pred = \
+        train_and_predict(xTr, yTr, xVal, yVal, xTe, algorithm=args.algorithm)
     submit(test_pred)
 
 
